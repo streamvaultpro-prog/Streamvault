c923a1f0bfd604c0bdeceb6cbebe3e818e037b4c
streamvaultpro-prog <streamvaultpro@gmail.com>
2026-03-25 00:25:03 -0500

    Initialize React application with StreamVaultPlatform

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import StreamVaultPlatform from './StreamVaultPlatform.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <StreamVaultPlatform />
+  </React.StrictMode>
+)
