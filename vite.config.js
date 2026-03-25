662d003abb06a5f0279e45007fab54e0e96db2c4
streamvaultpro-prog <streamvaultpro@gmail.com>
2026-03-25 01:15:57 -0500

    Configure Vite build options with Rollup input

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html')
+    }
+  }
 })
